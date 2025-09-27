92f0367248b30dd1aeb414e0b11c491542ff7a4c
VZB555 <victor.zaccaro@gmail.com>
2025-09-27 21:27:31 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // === SERVEUR HTTP POUR LE FRONTEND ===
 app.use(express.static(path.join(__dirname, 'public')));
