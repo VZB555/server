d374a6f05e0dd068850e18dd1325b5c8e0d4d984
VZB555 <victor.zaccaro@gmail.com>
2025-10-11 14:42:38 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -56,7 +56,9 @@ wss.on('connection', (ws, req) => {
       // Message du navigateur → envoyer à l'Arduino
       else if (data.type === 'command') {
         if (arduinoSocket && arduinoSocket.readyState === WebSocket.OPEN) {
-          arduinoSocket.send(JSON.stringify({ type: 'command', payload: data.payload }));
+          console.log("message du brower recu)";
+		  console.log(data.payload);
+		  arduinoSocket.send(JSON.stringify({ type: 'command', payload: data.payload }));
         }
       }
 
